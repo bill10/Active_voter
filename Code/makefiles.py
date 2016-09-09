a99da8a0b2ba66fdeaacad1f542971fbc3107cb6
bill10 <bill@Bills-MacBook-Pro.local>
2016-09-09 16:31:49 +0800

    how to compile

--- Code/makefiles.py
+++ Code/makefiles.py
@@ -1,13 +1,15 @@
 import numpy as np
 import subprocess
 
+IGRAPH_INCLUDE=/usr/local/Cellar/igraph/0.7.1/include/igraph#/usr/local/include/igraph
+IGRAPH_LIB=/usr/local/Cellar/igraph/0.7.1/lib#/usr/local/lib
 count=0;
 node=10000;
 edge=20000;
 g=2;
 U0=1.0/g*np.ones(g,);
 U0[-1]=1-sum(U0[:-1]);
-#system('g++ main.cpp Dynamic_Voter.h Dynamic_Voter.cpp Node.cpp Edge.cpp Random1.cpp -O3 -Wall -I/usr/local/include/igraph -L/usr/local/lib -ligraph -o DynamicVoter');
+subprocess.call('g++ main.cpp Dynamic_Voter.cpp Node.cpp Edge.cpp Random1.cpp -O3 -Wall -I/usr/local/Cellar/igraph/0.7.1/include/igraph -L/usr/local/Cellar/igraph/0.7.1/lib -ligraph -o active_voter');
 for realization in [1]:
     for alpha in [0.4]:
         for lambda in [0.4]:
